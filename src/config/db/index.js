5bfb1a13e6a022cde454d6b1d1d4cfd10fa2c6d4
unknown <hieu131517@gmail.com>
2022-02-22 14:55:24 +0700

    db

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 async function connect(){
     try {
-        await mongoose.connect('mongodb://localhost:27017/webserver',{
+        await mongoose.connect('mongodb+srv://webserver:xfiHOH98@cluster0.vxhdh.mongodb.net/webserver?retryWrites=true&w=majority',{
             useNewUrlParser: true,
             useUnifiedTopology:true
         });
